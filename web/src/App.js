e3d26dea2dd73c8d8b2e00685c13b00dbd4f768c
Nik <nerdvestor@gmail.com>
2021-07-13 21:22:31 -0700

    done

---
+++ web/src/App.js
@@ -0,0 +1,54 @@
+
+import axios from 'axios';
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [url, setUrl] = useState("")
+  const [sent, setSent] = useState(false)
+  const [name, setName] = useState("")
+
+  const sendUrl = async (e) => {
+    e.preventDefault()
+    setSent(true)
+
+    try {
+      await axios.post("http://localhost:4000/get_text", {
+        url,
+        name 
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  } 
+
+
+  return (
+    <div className="App">
+
+      {!sent ? (
+        <>
+          <h1>Enter Name for File and URL</h1>
+          <form onSubmit={sendUrl} className="form">
+            <input type="text" className="text-input" 
+            placeholder="Name you audio here" value={name}
+            onChange={(e) => setName(e.target.value)}
+             />
+            <input type="text" className="text-input" 
+            placeholder="Url goes here" value={url}
+            onChange={(e) => setUrl(e.target.value)}
+             />
+             <button type="submit">Create Audio</button>
+          </form>
+        </>
+      ): (
+        <h1>Audio Created</h1>
+      
+      )}
+      
+    </div>
+    
+  );
+}
+
+export default App;
